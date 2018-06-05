6707d76491c297018ebf4e10fdab19492537e357
Ivan <ivan.sherstnyuk@gmail.com>
2018-06-05 14:30:08 +0300

    Добавил проверки при создании и обновлении официанта

--- src/controllers/waiters.ts
+++ src/controllers/waiters.ts
@@ -1,5 +1,5 @@
 import {
-  Authorized,
+  Authorized, BadRequestError,
   Body,
   Delete,
   Get,
@@ -13,12 +13,16 @@ import {
 import {Repository} from "typeorm";
 import {Waiter} from "../entities/waiter";
 import {InjectRepository} from "typeorm-typedi-extensions";
+import {Cafe} from "../entities/cafe";
 
 @JsonController()
 export class WaitersController {
 
   @InjectRepository(Waiter)
   private repository: Repository<Waiter>;
+
+  @InjectRepository(Cafe)
+  private cafeRepository: Repository<Cafe>;
 
   @Authorized()
   @Get("/waiters")
@@ -37,16 +41,36 @@ export class WaitersController {
 
   @Authorized()
   @Post("/waiters")
-  create(@Body() waiter: Waiter) {
+  async create(@Body() waiter: Waiter) {
+    if (typeof waiter.cafe !== "number") {
+        throw new BadRequestError("Cafe must be a id number")
+    }
+
+    const existedCafe = await this.cafeRepository.findOne(waiter.cafe);
+
+    if(!existedCafe) {
+      throw new NotFoundError(`Cafe with id ${waiter.id} not found`)
+    }
+
     return this.repository.save(waiter);
   }
 
   @Authorized()
   @Put("/waiters/:id")
-  update(@Param("id") id: number, @Body() waiter: Waiter) {
+  async update(@Param("id") id: number, @Body() waiter: Waiter) {
     const existedWaiter = this.repository.findOne(id);
     if (!existedWaiter)
       throw new NotFoundError(`Официант с id: ${id} не найден`);
+
+    if (typeof waiter.cafe !== "number") {
+      throw new BadRequestError("Cafe must be a id number")
+    }
+
+    const existedCafe = await this.cafeRepository.findOne(waiter.cafe);
+
+    if(!existedCafe) {
+      throw new NotFoundError(`Cafe with id ${waiter.id} not found`)
+    }
 
     return this.repository.save({...existedWaiter, ...waiter, id: id});
   }
